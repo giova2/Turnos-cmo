17b685252d3bf4a039fb5dd193bb87921c89c7e3
franco giovannangelo <fcgiova@gmail.com>
2020-10-10 13:43:17 -0300

    Updates

---
+++ precache-manifest.0c4e2214dd9d802e03460c6dc581afa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8156300cb8df79f4a1bb3b00e60d07",
+    "url": "/Turnos-cmo/index.html"
+  },
+  {
+    "revision": "dc4c140e9df0e88a0cbc",
+    "url": "/Turnos-cmo/static/css/main.496261a1.chunk.css"
+  },
+  {
+    "revision": "0455bbb67085f08bc115",
+    "url": "/Turnos-cmo/static/js/2.bfe5d0cf.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Turnos-cmo/static/js/2.bfe5d0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4c140e9df0e88a0cbc",
+    "url": "/Turnos-cmo/static/js/main.c7f821f0.chunk.js"
+  },
+  {
+    "revision": "2d643f6c87d57bab8df8",
+    "url": "/Turnos-cmo/static/js/runtime-main.769adea7.js"
+  }
+]);
